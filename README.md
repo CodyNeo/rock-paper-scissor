bb6f415ada94bab0054a0b88bbed19677f9e0a34
CodyNeo <abdallahomm123@gmail.com>
2026-05-21 09:50:43 +0300

    made a rock paper scissor game
    you play against the computer for 5 rounds.

---
+++ README.md
@@ -0,0 +1,3 @@
+# Rock Paper Scissor
+This a project from [the odin project] (https://www.theodinproject.com/) for learning web development from scratch.
+this website is made to practice using javascript.
---
+++ my-script.js
@@ -0,0 +1,77 @@
+function getComputerGuess(){
+    let numGuess = Math.floor(Math.random() * 3) + 1;
+    console.log("the computer made their choice")
+    if (numGuess === 1){
+        return "rock"
+    } else if (numGuess === 2){
+        return "paper"
+    } else if (numGuess === 3){
+        return "scisssor"
+    }
+}
+
+function getHumanGuess(){
+    let humanGuess = prompt('Enter your choice:');
+    if(humanGuess === 'rock' || humanGuess === "Rock" || humanGuess === "1" || humanGuess === "ROCK"){
+        return "rock"
+    } else if (humanGuess === 'paper' || humanGuess === "Paper" || humanGuess === "2" || humanGuess === "PAPER"){
+        return "paper"
+    } else if (humanGuess === 'scissor' || humanGuess === "Scissor" || humanGuess === "3" || humanGuess === "SCISSOR"){
+        return "scissor"
+    }
+}
+
+function playRound(humanChoice, computerChoice){
+    console.log("The computer chose " + computerChoice + " and you chose " + humanChoice);
+    if (humanChoice === "rock"){
+        if (computerChoice === "rock"){
+            console.log("TIE! Nobody wins.");
+            return "tie";
+        } else if (computerChoice === "paper"){
+            console.log("You lose! Paper beats rock.");
+            return "computer";
+        } else if (computerChoice === "scissor"){
+            console.log("You win! Rock beats scisssor.");
+            return "human";
+        }
+    } else if (humanChoice === "paper"){
+        if (computerChoice === "paper"){
+            console.log("TIE! Nobody wins.");
+            return "tie";
+        } else if (computerChoice === "scissor"){
+            console.log("You lose! Paper beats rock.");
+            return "computer";
+        } else if (computerChoice === "rock"){
+            console.log("You win! Rock beats scisssor.");
+            return "human";
+        }
+    } else if (humanChoice === "scissor"){
+        if (computerChoice === "scissor"){
+            console.log("TIE! Nobody wins.");
+            return "tie";
+        } else if (computerChoice === "rock"){
+            console.log("You lose! Paper beats rock.");
+            return "computer";
+        } else if (computerChoice === "paper"){
+            console.log("You win! Rock beats scisssor.");
+            return "human";
+        }
+    }
+}
+
+function playGame (){
+    let humanScore = 0;
+    let computerScore = 0;
+
+    for ( let i = 1 ; i <= 5 ; i++){
+        let computerChoice = getComputerGuess();
+        let humanChoice = getHumanGuess();
+        let winner = playRound(humanChoice, computerChoice);
+        if (winner === "human"){
+            humanScore++;
+        } else if(winner === "computer") {
+            computerScore++;
+        }
+    }
+}
+playGame()
